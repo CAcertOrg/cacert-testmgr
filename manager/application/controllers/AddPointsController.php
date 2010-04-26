c0b92575118607007447ed064e1b81c9bc5e4311
Michael Tänzer <neo@nhng.de>
2010-04-26 20:57:27 +0200

    Construct and display the AddPoints view
    * define input form (draft)
    * create the view and make it display the form
    * add a link to the view in the topNav
    
    Signed-off-by: Michael Tänzer <neo@nhng.de>

--- manager/application/controllers/AddPointsController.php
+++ manager/application/controllers/AddPointsController.php
@@ -12,6 +12,44 @@ class AddPointsController extends Zend_Controller_Action
     
     public function indexAction()
     {
-        /* TODO */
+        $this->view->assurance_form = $this->getAssuranceForm();
+        $this->render('index');
+    }
+    
+    public function assuranceAction()
+    {
+        /* Validate form */
+        if (!$this->getRequest()->isPost()) {
+            return $this->_forward('index');
+        }
+        
+        $form = $this->getAssuranceForm();
+        if (!$form->isValid($_POST)) {
+            $this->view->assurance_form = $form;
+            return $this->render('index');
+        }
+        
+        
+        /* Form is valid -> get values and process them */
+        $values = $form->getValues();
+    }
+    
+    protected function getAssuranceForm()
+    {
+        $form = new Zend_Form();
+        $form->setAction('/add-points/assurance')->setMethod('post');
+        
+        $quantity = new Zend_Form_Element_Text('quantity');
+        $quantity->setRequired(true)
+                ->setLabel(I18n::_('Number of Points'))
+                ->addFilter(new Zend_Filter_Int())
+                ->addValidator(new Zend_Validate_Between(0, 100));
+        $form->addElement($quantity);
+        
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setLabel(I18n::_('Assure Me'));
+        $form->addElement($submit);
+        
+        return $form;
     }
 }
